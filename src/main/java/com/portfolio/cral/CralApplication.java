9b627113c6b429563fe452c1a3071181db2ee3f2
Carlos Roberto Arando Llanos <chalys.all@gmail.com>
2023-04-26 08:48:15 -0400

    Creación del proyecto - portfolio-BackEnd
    Creación y configuración del proyecto Back-End en Spring Boot y Netbeans

---
+++ src/main/java/com/portfolio/cral/CralApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.cral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/cral/CralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.cral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
