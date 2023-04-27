df9b790ea102d077691887e92ef8a33414606dca
Carlos Roberto Arando Llanos <chalys.all@gmail.com>
2023-04-27 01:46:01 -0400

    DockeFile

--- src/main/java/com/portfolio/cral/Security/Controller/Mensaje.java
+++ src/main/java/com/portfolio/cral/Security/Controller/Mensaje.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.portfolio.cral.Security.Controller;
 
 /**
@@ -9,18 +5,18 @@ package com.portfolio.cral.Security.Controller;
  * @author Carlos
  */
 public class Mensaje {
-    private String mensaje;
-    
-    //Constructor
 
+    private String mensaje;
+
+    //Constructor
     public Mensaje() {
     }
 
     public Mensaje(String mensaje) {
         this.mensaje = mensaje;
     }
+    
     //Getter y Setter
-
     public String getMensaje() {
         return mensaje;
     }
@@ -28,5 +24,5 @@ public class Mensaje {
     public void setMensaje(String mensaje) {
         this.mensaje = mensaje;
     }
-    
+
 }
