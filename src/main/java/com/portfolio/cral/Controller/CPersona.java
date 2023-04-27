ea3577cd74798fe6b04d9283f47cb94c819bc4a0
Carlos Roberto Arando Llanos <chalys.all@gmail.com>
2023-04-27 17:20:44 -0400

    Verificación de URL /

--- src/main/java/com/portfolio/cral/Controller/CPersona.java
+++ src/main/java/com/portfolio/cral/Controller/CPersona.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"https://frontendcral.web.app"})
+@CrossOrigin(origins = {"https://frontendcral.web.app/"})
 
 public class CPersona {
 
